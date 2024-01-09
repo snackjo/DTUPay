cbdb6e133c03aed305ca09c730785286f7c1281d
snackjo <carlj0908@gmail.com>
2024-01-09 11:23:46 +0100

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+chmod +x build.sh
 ./build.sh
 
 # Update the set of services and
 # build and execute the system tests
 pushd end-to-end-tests
+chmod +x deploy.sh
 ./deploy.sh 
 sleep 5
+chmod +x test.sh
 ./test.sh
 popd
 
