b21e31644301236816124815e2c65f1c5bdbe8ba
snackjo <carlj0908@gmail.com>
2024-01-10 16:56:56 +0100

    Added token-service to deploy.sh

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -3,5 +3,5 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitMq
 sleep 20
-docker-compose up -d dtu-pay-service account-service
+docker-compose up -d dtu-pay-service account-service token-service
 
