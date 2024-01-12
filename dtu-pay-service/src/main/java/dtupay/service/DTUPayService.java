5743bf1d2838b9e4128cfce8ce9f60580847997d
snackjo <carlj0908@gmail.com>
2024-01-12 15:19:41 +0100

    Different todo description

--- dtu-pay-service/src/main/java/dtupay/service/DTUPayService.java
+++ dtu-pay-service/src/main/java/dtupay/service/DTUPayService.java
@@ -87,7 +87,7 @@ public class DTUPayService {
     private void handlePaymentCompleted(Event event) {
         CorrelationId correlationId = event.getArgument(0, CorrelationId.class);
 
-        //TODO End-to-end test stops here, event "PaymentRequested
+        //TODO End-to-end test never reaches this point
         paymentCorrelations.get(correlationId).join();
         paymentCorrelations.remove(correlationId);
     }
