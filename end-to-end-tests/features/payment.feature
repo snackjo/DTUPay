be0cb6769e41c8a17cfd98ebab7feb6b7542c73a
Noblix <oliver@stenberg-jakobsen.dk>
2024-01-18 00:35:13 +0100

    Add E2E test with two simultaneous payments

--- end-to-end-tests/features/payment.feature
+++ end-to-end-tests/features/payment.feature
@@ -1,14 +1,34 @@
 Feature: Payment feature
     Scenario: Customer pays merchant successfully
-        Given merchant registered in bank with a balance of 1000
-        And the merchant is also registered with DTUPay
-        Given customer registered in bank with a balance of 1000
-        And the customer is also registered with DTUPay
-        And the customer has generated tokens
-        Given the merchant has received a token from the customer
-        When the merchant requests a payment of 100 using the token
+        Given the merchant "Alice" registered in bank with a balance of 1000
+        And the merchant "Alice" is also registered with DTUPay
+        Given the customer "Bob" registered in bank with a balance of 1000
+        And the customer "Bob" is also registered with DTUPay
+        And "Bob" has generated tokens
+        Given "Alice" has received a token from "Bob"
+        When "Alice" requests a payment of 100
         Then the payment is successful
-        And the merchant's balance is 1100
-        And the customer's balance is 900
+        And the merchant "Alice" balance is 1100
+        And the customer "Bob" balance is 900
 
-
+    Scenario: Two different payments at the same time
+        Given the merchant "Alice" registered in bank with a balance of 1000
+        And the merchant "Alice" is also registered with DTUPay
+        Given the customer "Bob" registered in bank with a balance of 1000
+        And the customer "Bob" is also registered with DTUPay
+        And "Bob" has generated tokens
+        Given the merchant "Charlie" registered in bank with a balance of 1000
+        And the merchant "Charlie" is also registered with DTUPay
+        Given the customer "Dave" registered in bank with a balance of 1000
+        And the customer "Dave" is also registered with DTUPay
+        And "Dave" has generated tokens
+        Given "Alice" has received a token from "Bob"
+        And "Charlie" has received a token from "Dave"
+        When "Alice" wants to request a payment of 100
+        And "Charlie" wants to request a payment of 500
+        And the two payments are requested at the same time
+        Then the two payments are successful
+        And the merchant "Alice" balance is 1100
+        And the customer "Bob" balance is 900
+        And the merchant "Charlie" balance is 1500
+        And the customer "Dave" balance is 500
--- end-to-end-tests/src/test/java/behaviourtests/PaymentSteps.java
+++ end-to-end-tests/src/test/java/behaviourtests/PaymentSteps.java
@@ -2,6 +2,7 @@ package behaviourtests;
 
 import CustomerApp.Customer;
 import CustomerApp.CustomerDtuPay;
+import CustomerApp.DtuPayException;
 import CustomerApp.Token;
 import MerchantApp.Merchant;
 import MerchantApp.MerchantDtuPay;
@@ -17,100 +18,145 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.assertEquals;
 
 public class PaymentSteps {
-    Customer customer;
-    Merchant merchant;
-    Token tokenReceivedFromCustomer;
+    private final Map<String, Customer> customers = new ConcurrentHashMap<>();
+    private final Map<String, Merchant> merchants = new ConcurrentHashMap<>();
+    private final Map<String, Token> tokenReceivedFromCustomer = new ConcurrentHashMap<>();
     private final BankService bank = new BankServiceService().getBankServicePort();
     private final MerchantDtuPay merchantDtuPay = new MerchantDtuPay();
     private final CustomerDtuPay customerDtuPay = new CustomerDtuPay();
-    private String paymentResponse;
+    private final Map<String, String> paymentResponses = new ConcurrentHashMap<>();
+    private final List<Thread> threads = new ArrayList<>();
 
-    @Given("merchant registered in bank with a balance of {int}")
-    public void merchantRegisteredInBankWithABalanceOf(int startingBalance) throws BankServiceException_Exception {
-        merchant = new Merchant();
-        merchant.setCprNumber("merchant1-17");
-        merchant.setFirstName("firstName1");
+    @Given("the merchant {string} registered in bank with a balance of {int}")
+    public void theMerchantRegisteredInBankWithABalanceOf(String firstName, int startingBalance) throws BankServiceException_Exception {
+        Merchant merchant = new Merchant();
+        merchant.setCprNumber(firstName + "-17");
+        merchant.setFirstName(firstName);
         merchant.setLastName("lastName1");
         BigDecimal balance = new BigDecimal(startingBalance);
 
         String accountId = bank.createAccountWithBalance(MapperUtility.merchantToUser(merchant), balance);
         merchant.setAccountId(accountId);
+        merchants.put(firstName, merchant);
     }
 
-    @And("the merchant is also registered with DTUPay")
-    public void theMerchantIsAlsoRegisteredWithDTUPay() {
-        merchant.setDtuPayId(merchantDtuPay.registerMerchant(merchant).getDtuPayId());
+    @And("the merchant {string} is also registered with DTUPay")
+    public void theMerchantIsAlsoRegisteredWithDTUPay(String firstName) {
+        merchants.get(firstName).setDtuPayId(merchantDtuPay.registerMerchant(merchants.get(firstName)).getDtuPayId());
     }
 
-    @Given("customer registered in bank with a balance of {int}")
-    public void customerRegisteredInBankWithABalanceOf(int startingBalance) throws BankServiceException_Exception {
-        customer = new Customer();
-        customer.setCprNumber("customer1-17");
-        customer.setFirstName("firstName1");
+    @Given("the customer {string} registered in bank with a balance of {int}")
+    public void theCustomerRegisteredInBankWithABalanceOf(String firstName, int startingBalance) throws BankServiceException_Exception {
+        Customer customer = new Customer();
+        customer.setCprNumber(firstName + "-17");
+        customer.setFirstName(firstName);
         customer.setLastName("lastName1");
         BigDecimal balance = new BigDecimal(startingBalance);
 
         String accountId = bank.createAccountWithBalance(MapperUtility.costumerToUser(customer), balance);
         customer.setAccountId(accountId);
+        customers.put(firstName, customer);
     }
 
-    @And("the customer is also registered with DTUPay")
-    public void theCustomerIsAlsoRegisteredWithDTUPay() {
-        customer.setDtuPayId(customerDtuPay.registerCustomer(customer).getDtuPayId());
+    @And("the customer {string} is also registered with DTUPay")
+    public void theCustomerIsAlsoRegisteredWithDTUPay(String firstName) {
+        customers.get(firstName).setDtuPayId(customerDtuPay.registerCustomer(customers.get(firstName)).getDtuPayId());
     }
 
-    @And("the customer has generated tokens")
-    public void theCustomerHasGeneratedTokens() throws Exception {
-        List<Token> tokens = customerDtuPay.requestTokens(customer, 5);
-        customer.setTokens(tokens);
+    @And("{string} has generated tokens")
+    public void hasGeneratedTokens(String firstName) throws DtuPayException {
+        List<Token> tokens = customerDtuPay.requestTokens(customers.get(firstName), 5);
+        customers.get(firstName).setTokens(tokens);
     }
 
-    @Given("the merchant has received a token from the customer")
-    public void theMerchantHasReceivedATokenFromTheCustomer() throws Exception {
-        tokenReceivedFromCustomer = customer.provideToken();
+    @Given("{string} has received a token from {string}")
+    public void hasReceivedATokenFrom(String merchantFirstName, String customerFirstName) throws Exception {
+        tokenReceivedFromCustomer.put(merchantFirstName, customers.get(customerFirstName).provideToken());
     }
 
-    @When("the merchant requests a payment of {int} using the token")
-    public void theMerchantRequestsAPaymentOfUsingTheToken(int paymentAmount) throws Exception {
-        MerchantApp.Token token = MapperUtility.mapCustomerTokenToMerchantToken(tokenReceivedFromCustomer);
-        paymentResponse = merchantDtuPay.requestPayment(paymentAmount, token, merchant.getDtuPayId());
+    @When("{string} requests a payment of {int}")
+    public void requestsAPaymentOf(String merchantFirstName, int paymentAmount) throws Exception {
+        MerchantApp.Token token = MapperUtility.mapCustomerTokenToMerchantToken(tokenReceivedFromCustomer.get(merchantFirstName));
+        paymentResponses.put(
+                merchantFirstName,
+                merchantDtuPay.requestPayment(
+                        paymentAmount,
+                        token,
+                        merchants.get(merchantFirstName).getDtuPayId()));
+    }
+
+    @When("{string} wants to request a payment of {int}")
+    public void wantsToRequestAPaymentOf(String merchantFirstName, int paymentAmount) {
+        MerchantApp.Token token = MapperUtility.mapCustomerTokenToMerchantToken(tokenReceivedFromCustomer.get(merchantFirstName));
+        Thread thread = new Thread(() -> {
+            try {
+                paymentResponses.put(
+                        merchantFirstName,
+                        merchantDtuPay.requestPayment(
+                                paymentAmount,
+                                token,
+                                merchants.get(merchantFirstName).getDtuPayId()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        threads.add(thread);
+    }
+
+    @And("the two payments are requested at the same time")
+    public void theTwoPaymentsAreRequestedAtTheSameTime() {
+        for (Thread thread : threads) {
+            thread.start();
+        }
     }
 
     @Then("the payment is successful")
     public void thePaymentIsSuccessful() {
-        assertEquals("Success", paymentResponse);
+        assertEquals("Success", paymentResponses.values().toArray()[0]);
     }
 
-    @And("the merchant's balance is {int}")
-    public void theMerchantSBalanceIs(int expectedBalance) throws BankServiceException_Exception {
-        Account account = bank.getAccount(merchant.getAccountId());
+    @Then("the two payments are successful")
+    public void theTwoPaymentsAreSuccessful() throws InterruptedException {
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        assertEquals(2, paymentResponses.values().size());
+        for (String paymentResponse : paymentResponses.values()) {
+            assertEquals("Success", paymentResponse);
+        }
+    }
+
+    @And("the merchant {string} balance is {int}")
+    public void theMerchantBalanceIs(String firstName, int expectedBalance) throws BankServiceException_Exception {
+        Account account = bank.getAccount(merchants.get(firstName).getAccountId());
         assertEquals(BigDecimal.valueOf(expectedBalance), account.getBalance());
     }
 
-    @And("the customer's balance is {int}")
-    public void theCustomerSBalanceIs(int expectedBalance) throws BankServiceException_Exception {
-        Account account = bank.getAccount(customer.getAccountId());
+    @And("the customer {string} balance is {int}")
+    public void theCustomerBalanceIs(String firstName, int expectedBalance) throws BankServiceException_Exception {
+        Account account = bank.getAccount(customers.get(firstName).getAccountId());
         assertEquals(BigDecimal.valueOf(expectedBalance), account.getBalance());
     }
 
     @After
     public void cleanBankAccounts() {
-        Customer[] customersToRetire = new Customer[]{ customer };
-        Merchant[] merchantsToRetire = new Merchant[]{ merchant };
-
-        for (Customer customerToRetire : customersToRetire) {
+        for (Customer customerToRetire : customers.values()) {
             try {
                 bank.retireAccount(customerToRetire.getAccountId());
             } catch (Exception ignored) {
             }
         }
 
-        for (Merchant merchantToRetire : merchantsToRetire) {
+        for (Merchant merchantToRetire : merchants.values()) {
             try {
                 bank.retireAccount(merchantToRetire.getAccountId());
             } catch (Exception ignored) {
